09512399d5f2efdd11e4216437af69b54e8e9589
Amaya Wickramaarachchi <apiyumi7@email.com>
2025-02-01 17:20:01 +0530

    Login button on login page linked to Explore page because login button doesn't redirect to profile page.

--- app/src/main/java/com/example/myapplication/page11.java
+++ app/src/main/java/com/example/myapplication/page11.java
@@ -15,10 +15,10 @@ public class page11 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_page11);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+      /*  ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
+        });*/
     }
 }
