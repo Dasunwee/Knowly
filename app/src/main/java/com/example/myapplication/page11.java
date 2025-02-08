ccab9fd379c1a291d4a14b8513225bdf9418ef1e
Amaya Wickramaarachchi <apiyumi7@email.com>
2025-02-08 12:58:50 +0530

    Updated page11.java

--- app/src/main/java/com/example/myapplication/page11.java
+++ app/src/main/java/com/example/myapplication/page11.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,18 @@ public class page11 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_page11);
-      /*  ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });*/
-    }
-}
+
+        // Find the button by its correct ID
+        View startButton = findViewById(R.id.getStartButton);
+
+        if (startButton != null) {
+            ViewCompat.setOnApplyWindowInsetsListener(startButton, (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
+        } else {
+            Log.e("page11", "Error: getStartButton not found in activity_page11.xml");
+        }
+    }}
+
