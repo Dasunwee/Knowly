392d56c615ccc864c3e96222450641ae31475282
imashiuthpala <imashiuthpala71@gmail.com>
2025-02-08 15:46:08 +0530

    change code of page6.java

--- app/src/main/java/com/example/myapplication/page6.java
+++ app/src/main/java/com/example/myapplication/page6.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
+79import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class page6 extends AppCompatActivity {
