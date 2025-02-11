7b884f89888307d7125c47205f92e0ed240d8f85
Dasun Weerawardhana <dasunweeravardhana@gmail.com>
2025-02-11 09:57:07 +0530

    Designed resources

--- app/src/main/java/com/example/myapplication/Page9.java
+++ app/src/main/java/com/example/myapplication/Page9.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Page9 extends AppCompatActivity {
 
+    private ImageButton profile;
+    private ImageButton home;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_page9);
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });*/
+
+        // Initialize UI components
+        home = findViewById(R.id.home_icon);
+        profile = findViewById(R.id.btn_profile);
+
+        // Set click listeners
+        home.setOnClickListener(v -> navigateTo(page5.class));
+        profile.setOnClickListener(v -> navigateTo(page3.class));
+    }
+    // Utility method for navigation
+    private void navigateTo(Class<?> targetPage) {
+        startActivity(new Intent(Page9.this, targetPage));
     }
 }
